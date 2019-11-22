7b2afca2a9fc2fa369e382e64a442a6d9720d55e
Bill Baker <bill.baker@synapsewireless.com>
2019-11-21 22:18:06 -0600

    Day 1, part 2.

--- 2018/day_1.py
+++ 2018/day_1.py
@@ -1,20 +1,44 @@
 class FrequencyTuner:
     def __init__(self):
         self.BASE_FRQ = 0
+        self.input_frq = []
+        self.iterations = 0
         self.adjusted_frq = self.BASE_FRQ
+        self.unique_frqs = set()
+        self.repeat_found = False
 
-    def adjust(self, variance: int) -> int:
+    def adjust(self, variance: int):
         self.adjusted_frq = self.adjusted_frq + variance
+        if self.adjusted_frq in self.unique_frqs:
+            if not self.repeat_found:
+                print(f"First duplicate detected: {self.adjusted_frq}")
+                self.repeat_found = True
+        else:
+            self.unique_frqs.add(self.adjusted_frq)
+
+    def read_input(self, file_path):
+        try:
+            with open(file_path, "r") as input_file:
+                self.input_frq = input_file.readlines()
+        except Exception:
+            print(f"Unable to read data from {file_path}")
+            exit()
+
+    def process_input(self):
+        while not self.repeat_found:
+            if self.iterations == 1:
+                print(f"First pass result: {self.adjusted_frq}")
+            for adjustment in self.input_frq:
+                try:
+                    adjustment = int(adjustment)
+                    self.adjust(adjustment)
+                except ValueError:
+                    print(f"Invalid data: {adjustment}")
+            self.iterations += 1
 
 
 if __name__ == "__main__":
-    complete = False
     frequency = FrequencyTuner()
-
-    while not complete:
-        try:
-            new_variance = int(input())
-            frequency.adjust(new_variance)
-        except ValueError:
-            print(f'Adjusted frequency is {frequency.adjusted_frq}.')
-            complete = True
+    frequency.read_input("frequencies.txt")
+    frequency.process_input()
+    print(f"Final result: {frequency.adjusted_frq}")
