ab49f59dc1c025c7fae66a31f179379f8acb4874
Bill Baker <bill.baker@synapsewireless.com>
2021-12-05 12:11:21 -0600

    Day 2 part 2.

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -1,10 +1,12 @@
 class Position:
     def __init__(self):
         data_file = open("inputs/day_2_input.txt", "r")
+        # data_file = open("inputs/sample.txt", "r")
         self.position_data = data_file.readlines()
         data_file.close()
         self.horizontal = 0
         self.depth = 0
+        self.aim = 0
 
     def plot_movement(self, movement):
         change = movement.split()
@@ -20,6 +22,24 @@ class Position:
         if direction == "up":
             self.depth -= distance
 
+    def plot_aim_movement(self, movement):
+        change = movement.split()
+        direction = change[0]
+        distance = int(change[1])
+
+        if direction == "forward":
+            self.horizontal += distance
+            if self.aim != 0:
+                self.depth += distance * self.aim
+        if direction == "backward":
+            self.horizontal -= distance
+            if self.aim != 0:
+                self.depth -= distance * self.aim
+        if direction == "down":
+            self.aim += distance
+        if direction == "up":
+            self.aim -= distance
+
     def follow_course(self):
         for movement in self.position_data:
             self.plot_movement(movement)
@@ -28,8 +48,18 @@ class Position:
 
         return location
 
+    def follow_aim_course(self):
+        for movement in self.position_data:
+            self.plot_aim_movement(movement)
+
+        print(f"Location is {self.horizontal} and depth is {self.depth}")
+        location = self.horizontal * self.depth
+
+        return location
+
 
 if __name__ == "__main__":
     course = Position()
 
-    print(f"Location is {course.follow_course()}")
+    # print(f"Location is {course.follow_course()}")
+    print(f"Location is {course.follow_aim_course()}")
