d52ab2862fc06f4c3ef3266117abb353e07a5f9c
Bill Baker <bill.baker@synapsewireless.com>
2021-12-05 11:10:45 -0600

    Day 1 part 2.

--- 2021/day_1_first.py
+++ 2021/day_1_first.py
@@ -16,8 +16,33 @@ class SonarData:
                 init = False
             else:
                 if reading > previous:
-                    count +=1
+                    count += 1
                 previous = reading
+
+        return count
+
+    def find_windowed_increases(self):
+        init = True
+        alpha = None
+        bravo = None
+        charlie = None
+        count = 0
+        previous = 0
+
+        alpha = int(self.sonar_data[0])
+        bravo = int(self.sonar_data[1])
+        charlie = int(self.sonar_data[2])
+        previous_window = alpha + bravo + charlie
+
+        for reading in self.sonar_data:
+            reading = int(reading)
+            new_window = bravo + charlie + reading
+            if new_window > previous_window:
+                count += 1
+
+            previous_window = new_window
+            bravo = charlie
+            charlie = reading
 
         return count
 
@@ -26,4 +51,4 @@ if __name__ == "__main__":
     sonar = SonarData()
 
     print(f"There are {sonar.find_increases()} depth increases.")
-
+    print(f"There are {sonar.find_windowed_increases()} window increases.")
