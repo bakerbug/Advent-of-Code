e26c9629ce362d39530bb38bfbc4493e49fb2ef1
Bill Baker <bill.baker@synapsewireless.com>
2021-12-06 15:18:27 -0600

    Day 4 part 2.

--- 2021/day_4.py
+++ 2021/day_4.py
@@ -10,49 +10,44 @@ class Bingo:
         data_file.close()
         self.boards = []
         self.results = []
+        self.winning_boards = []
         self.parse_board_data()
 
-    def check_rows(self):
-        board_id = 0
-        for board in self.results:
-            row_id = 0
-            for row in board:
-                winner = True
-                number_id = 0
-                for number in row:
-                    if number is not True:
-                        winner = False
-                if winner:
-                    return board_id
-            board_id += 1
-        return -1
+    def check_rows(self, board):
+        row_id = 0
+        for row in board:
+            winner = True
+            number_id = 0
+            for number in row:
+                if number is not True:
+                    winner = False
+            if winner:
+                return True
+        return False
 
-    def check_columns(self):
-        board_id = 0
+    def check_columns(self, board):
         column_width = len(self.results[0][0])
-
-        for board in self.results:
-
-            for column_id in range(column_width):
-                winner = True
-                for row in board:
-                    if row[column_id] is not True:
-                        winner = False
-                if winner:
-                    return board_id
-            board_id += 1
-        return -1
+        for column_id in range(column_width):
+            winner = True
+            for row in board:
+                if row[column_id] is not True:
+                    winner = False
+            if winner:
+                return True
+        return False
 
     def find_winning_board(self):
-        win_by_rows = self.check_rows()
-        if win_by_rows >= 0:
-            print(f"The winning board by rows is {win_by_rows}")
-            return win_by_rows
-        win_by_columns = self.check_columns()
-        if win_by_columns >= 0:
-            print(f"The winning board by columns is {win_by_columns}")
-            return win_by_columns
-        return -1
+        board_id = 0
+        winners = []
+        for board in self.results:
+            if board_id in self.winning_boards:
+                pass
+            else:
+                if self.check_rows(board) or self.check_columns(board):
+                    self.winning_boards.append(board_id)
+                    winners.append(board_id)
+            board_id += 1
+        return winners
 
     def parse_board_data(self):
         board = []
@@ -80,15 +75,18 @@ class Bingo:
     def add_move(self, move):
         board_id = 0
         for board in self.boards:
-            row_id = 0
-            for row in board:
-                number_id = 0
-                for number in row:
-                    if int(move) == int(number):
-                        print(f"Setting Board {board_id}, Row {row_id}, Number {number_id}")
-                        self.results[board_id][row_id][number_id] = True
-                    number_id += 1
-                row_id += 1
+            if board_id in self.winning_boards:
+                pass
+            else:
+                row_id = 0
+                for row in board:
+                    number_id = 0
+                    for number in row:
+                        if int(move) == int(number):
+                            # print(f"Setting Board {board_id}, Row {row_id}, Number {number_id}")
+                            self.results[board_id][row_id][number_id] = True
+                        number_id += 1
+                    row_id += 1
             board_id += 1
 
     def find_sum_of_board(self, board_id):
@@ -106,23 +104,26 @@ class Bingo:
 
     def play_games(self):
         for move in self.move_data:
-            self.add_move(move)
-            winning_board = self.find_winning_board()
-
-            if winning_board >= 0:
-                board_sum = self.find_sum_of_board(winning_board)
-                print(f"The winning board sum is {board_sum}")
-                print(f"The final score is {board_sum * int(move)}")
+            if len(self.winning_boards) == len(self.boards):
+                print("All boards have won!")
                 exit()
+            self.add_move(move)
+            winning_boards = self.find_winning_board()
+
+            for winning_board in winning_boards:
+                if winning_board >= 0:
+                    board_sum = self.find_sum_of_board(winning_board)
+                    print(f"The winning board is {winning_board}")
+                    print(f"The winning board sum is {board_sum}")
+                    print(f"The last move was {move}")
+                    print(f"The final score is {board_sum * int(move)}")
+                    print(f"The winners list is: {self.winning_boards}\n")
+
+                    for row in self.results[winning_board]:
+                        print(row)
+                    print("\n")
 
 
 if __name__ == "__main__":
     game = Bingo()
-
-    print(game.boards)
-    print(game.results)
-
     game.play_games()
-    game.results[0][0][0] = True
-
-    print(game.results)
