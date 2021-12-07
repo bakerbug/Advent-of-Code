3ae9772a28f88bc1b76208654f2326f01378edd6
Bill Baker <bill.baker@synapsewireless.com>
2021-12-07 11:10:06 -0600

    Day 7 part 2.

--- 2021/day_7.py
+++ 2021/day_7.py
@@ -20,9 +20,23 @@ class Crabs:
             total_cost += abs(position - crab)
         return total_cost
 
+    def find_advanced_fuel_cost(self, position):
+        total_cost = 0
+        for crab in self.data:
+            basic_cost = abs(position - crab)
+            penalty = 0
+            total_cost += basic_cost
+            for this_step in range(basic_cost):
+                total_cost += penalty
+                penalty += 1
+        return total_cost
+
     def find_costs(self):
-        for best_guess in self.positions.most_common():
-            self.position_costs.append(self.find_fuel_cost(best_guess[0]))
+        for best_guess in range(max(self.data)):  # Using Counter ultimately was a big mistake.
+            this_cost = self.find_advanced_fuel_cost(best_guess)
+            self.position_costs.append(this_cost)
+            print(f"Cost for position {best_guess} is {this_cost}")
+        # self.position_costs.append(self.find_advanced_fuel_cost(5))
 
     def find_lowest_cost(self):
         self.find_costs()
