d5297dc6c777d62e1183abbb6d82edf9f53ad492
Bill Baker <bill.baker@synapsewireless.com>
2021-12-07 08:50:54 -0600

    Day 7 part 1.

---
+++ 2021/day_7.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+
+class Crabs:
+    def __init__(self):
+        with open("inputs/day_7_inputs.txt", "r") as data_file:
+            raw_data = data_file.readline()
+        raw_data = raw_data.rstrip("\n")
+        raw_data = raw_data.split(",")
+        self.data = []
+        for item in raw_data:
+            self.data.append(int(item))
+
+        self.positions = Counter(self.data)
+        self.position_costs = []
+
+    def find_fuel_cost(self, position):
+        total_cost = 0
+        for crab in self.data:
+            total_cost += abs(position - crab)
+        return total_cost
+
+    def find_costs(self):
+        for best_guess in self.positions.most_common():
+            self.position_costs.append(self.find_fuel_cost(best_guess[0]))
+
+    def find_lowest_cost(self):
+        self.find_costs()
+        best_cost = min(self.position_costs)
+        print(f"Lowest fuel cost is {best_cost}")
+
+
+if __name__ == "__main__":
+    swarm = Crabs()
+    swarm.find_lowest_cost()
