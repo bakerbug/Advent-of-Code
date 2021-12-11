865bf01cdc1684ca8e94c6a6259e7ec282e28ba7
Bill Baker <bill.baker@synapsewireless.com>
2021-12-11 13:26:28 -0600

    Day 10 part 2.

--- 2021/day_10.py
+++ 2021/day_10.py
@@ -19,7 +19,7 @@ class Syntax:
                     stack.append(cmd)
                 elif cmd == ")":
                     cmd_open = stack.pop()
-                    expected_open = self.find_open(cmd)
+                    expected_open = self.find_opposite(cmd)
 
                     if cmd_open != expected_open:
                         print(f"Corrupt line: {cmd_open} != {cmd}")
@@ -27,7 +27,7 @@ class Syntax:
                         is_clean = False
                 elif cmd == "]":
                     cmd_open = stack.pop()
-                    expected_open = self.find_open(cmd)
+                    expected_open = self.find_opposite(cmd)
 
                     if cmd_open != expected_open:
                         print(f"Corrupt line: {cmd_open} != {cmd}")
@@ -35,7 +35,7 @@ class Syntax:
                         is_clean = False
                 elif cmd == "}":
                     cmd_open = stack.pop()
-                    expected_open = self.find_open(cmd)
+                    expected_open = self.find_opposite(cmd)
 
                     if cmd_open != expected_open:
                         print(f"Corrupt line: {cmd_open} != {cmd}")
@@ -43,7 +43,7 @@ class Syntax:
                         is_clean = False
                 elif cmd == ">":
                     cmd_open = stack.pop()
-                    expected_open = self.find_open(cmd)
+                    expected_open = self.find_opposite(cmd)
 
                     if cmd_open != expected_open:
                         print(f"Corrupt line: {cmd_open} != {cmd}")
@@ -53,30 +53,67 @@ class Syntax:
             if is_clean:
                 self.cleaned_code.append(line)
 
-        print(f"Error score: {self.error_score}")
+        print(f"Error score: {self.error_score}\n")
 
     def complete_code(self):
+        score_list = []
         for line in self.cleaned_code:
-            pass
+            stack = []
+            line_score = 0
+            for cmd in list(line):
+                if cmd == "<" or cmd == "(" or cmd == "{" or cmd == "[":
+                    stack.append(cmd)
+                elif cmd == ">" or cmd == ")" or cmd == "}" or cmd == "]":
+                    stack.pop()
 
+            # print(f"stack: {stack}")
 
+            for index in range(len(stack)):
+                cmd = stack.pop()
+                close = self.find_opposite(cmd)
+                line_score = (line_score * 5) + self.character_score(close)
 
-    def find_open(self, cmd_close):
-        if cmd_close == ")":
+            # print(f"Line score: {line_score}")
+            score_list.append(line_score)
+        score_list.sort()
+        # print(f"Score List: {score_list}")
+        score_len = len(score_list)
+        mid_point = round(score_len / 2)
+
+        print(f"Final Score: {score_list[mid_point]}")
+
+    def character_score(self, char):
+        if char == ")":
+            return 1
+        elif char == "]":
+            return 2
+        elif char == "}":
+            return 3
+        elif char == ">":
+            return 4
+
+    def find_opposite(self, command):
+        if command == ")":
             return "("
-        elif cmd_close == "]":
+        elif command == "]":
             return "["
-        elif cmd_close == "}":
+        elif command == "}":
             return "{"
-        elif cmd_close == ">":
+        elif command == ">":
             return "<"
+        elif command == "(":
+            return ")"
+        elif command == "[":
+            return "]"
+        elif command == "{":
+            return "}"
+        elif command == "<":
+            return ">"
         else:
-            print(f"WTF is {cmd_close}?")
-
-
-
+            print(f"WTF is {command}?")
 
 
 if __name__ == "__main__":
     syn = Syntax()
     syn.clean_code()
+    syn.complete_code()
